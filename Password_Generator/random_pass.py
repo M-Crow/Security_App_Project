0f21f51d3b4de62b3206891c3b67e3f6c61b1860
M-Crow <Mcrowbb24@gmail.com>
2022-05-06 13:00:33 -0400

    added if statement

--- Password_Generator/random_pass.py
+++ Password_Generator/random_pass.py
@@ -5,9 +5,13 @@ letter_list = list(string.ascii_letters)
 number_list = list(string.digits)
 charecter_list = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
+
 def random_password():
     random_pass = ""
     user_input = int(input("How long would you like your password to be? Please enter a number 1-20: "))
+    if user_input > int(20) or user_input < int(1):
+        print("You did not enter a valid number")
+        user_input = int(input("Please enter a new number: "))
     for i in range(user_input):
         random_pass += random.choice(letter_list + number_list + charecter_list)
     return "Your password is: " + random_pass
