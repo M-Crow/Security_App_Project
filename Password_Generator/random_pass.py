e3087bda34da78aa5f69746285d51228c6ff4728
M-Crow <Mcrowbb24@gmail.com>
2022-05-06 14:08:53 -0400

    final commit of day

--- Password_Generator/random_pass.py
+++ Password_Generator/random_pass.py
@@ -6,13 +6,16 @@ number_list = list(string.digits)
 charecter_list = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
 def random_password():
-    random_pass = ""
-    user_input = int(input("How long would you like your password to be? Please enter a number 1-20: "))
-    if user_input > int(20) or user_input < int(1):
+    
+    password = ""
+    
+    password_length = int(input("How long would you like your password to be? Please enter a number 1-20: "))
+    
+    if password_length > int(20) or password_length < int(1):
         print("You did not enter a valid number")
-        user_input = int(input("Please enter a new number: "))
-    for i in range(user_input):
-        random_pass += random.choice(letter_list + number_list + charecter_list)
-    return "Your password is: " + random_pass
+        password_length = int(input("Please enter a new number: "))
+    for i in range(password_length):
+        password += random.choice(letter_list + number_list + charecter_list)
+    return "Your password is: " + password
 
 print(random_password())
