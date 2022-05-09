f4617648831b4c8f49bc810a19c5ba575956b984
M-Crow <Mcrowbb24@gmail.com>
2022-05-09 11:55:15 -0400

    commit

---
+++ login_page.py
@@ -0,0 +1,6 @@
+# Login page for security hub app
+
+class User():
+    def __init__(self, login_username, login_password):
+        self.login_username = login_username
+        self.login_password = login_password 
